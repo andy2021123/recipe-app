fad31e66d300f3be6b91337a448a4210e079f55f
Andrew Kauffman <andrewkauffman15@gmail.com>
2024-04-29 22:07:15 -0400

    Added the ability to add selectors for a given domain

--- server/routes/selectors.js
+++ server/routes/selectors.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { runScraper, scrapeWebsite } from '../utils/scraper.js'
+import { addSelectors } from '../database/selectors.js'
 
 const router = express.Router()
 
@@ -9,6 +10,17 @@ router.post('/test', async (req, res) => {
   const recipe = await runScraper(selectors.url, selectors)
 
   res.send(recipe)
+})
+
+router.post('/', async (req, res) => {
+  const { body: selectors } = req
+  
+  try {
+    await addSelectors(selectors)
+    res.sendStatus(201)
+  } catch {
+    res.sendStatus(204)
+  }
 })
 
 export default router
